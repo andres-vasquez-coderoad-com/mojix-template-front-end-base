3a56372d97c44534a052912129d6eddb225d1a1d
andres-vasquez-coderoad-com <andres.vasquez@mojix.com>
2021-03-11 11:26:46 -0400

    3 files modified, 9 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component, VERSION } from '@angular/core';
+import { AfterViewInit, Component, VERSION } from "@angular/core";
+import { ApiService } from "./api/api.service";
+import { environment } from "../environments/environment";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements AfterViewInit {
+  /* ******** INTRUCTIONS **************
+     1. Fill the table with the info provided by the API
+     2. Modify the stylesheets to get the desired UI
+     3. Color the rows using an algorithm using the indexes of the loop. Choose one:
+        a) Fibonnacci sequence
+        b) Prime numbers
+        c) Odd/Even.
+     4. Modify the selector to support muti-language.
+
+     ******** EXTRAS **************
+     1. Use the Movie component to display the movie info
+     2. Use environment variables to fill the default values
+  */
+
+  constructor(private api: ApiService) {}
+
+  ngAfterViewInit(): void {
+    const apiKey = "1b501bbda107113acc653f328a2e935d";
+    const language = "en";
+    this.api.getMovies(apiKey, language).subscribe(
+      data => {
+        console.log("Data", data);
+      },
+      error => {}
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { MovieModule } from "./movie/movie.module";
+import { ApiService } from "./api/api.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule, MovieModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [ApiService]
 })
-export class AppModule { }
+export class AppModule {}
